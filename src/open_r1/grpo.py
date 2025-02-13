5fb9bc527e659449f74aa968c608fd20bb5fca66
tpoisonooo <khj.application@aliyun.com>
2025-02-13 19:07:21 +0800

    fix(grpo.py): update prompt

--- src/open_r1/grpo.py
+++ src/open_r1/grpo.py
@@ -97,12 +97,8 @@ class GRPOScriptArguments(ScriptArguments):
 
 
 SYSTEM_PROMPT = (
-    "A conversation between User and Assistant. The user asks a question, and the Assistant solves it. The assistant "
-    "first thinks about the reasoning process in the mind and then provides the user with the answer. The reasoning "
-    "process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., "
-    "<think> reasoning process here </think><answer> answer here </answer>"
+    "A conversation between user and assistant. The user asks a question, and the assistant solves it. The assistant first thinks about the reasoning process in the mind and then provides the user with the answer. The reasoning process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think><answer> answer here </answer>"
 )
-
 
 def main(script_args, training_args, model_args):
     # Set seed for reproducibility
